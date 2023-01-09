4a2c587fa33282bd4134a1cfc889aaef23f2cd66
Minalni789 <122231159+Minalni789@users.noreply.github.com>
2023-01-09 17:43:56 +0200

    Added Description

--- faces.py
+++ faces.py
@@ -1,3 +1,12 @@
+''' In a file called faces.py, implement a function called convert that accepts a str as input and returns
+that same input with any :) converted to 🙂 (otherwise known as a slightly smiling face) and any :( converted
+to 🙁 (otherwise known as a slightly frowning face). All other text should be returned unchanged.
+
+Then, in that same file, implement a function called main that prompts the user for input, calls convert on that 
+input, and prints the result. 
+'''
+
+
 def convert(variable):
 
     printed = print(variable.replace(":)", "🙂").replace(":(", "🙁"))
