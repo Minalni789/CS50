54fa355680c60d35bf05468fa068816762047f02
Minalni789 <122231159+Minalni789@users.noreply.github.com>
2023-01-09 18:09:35 +0200

    Added Description

--- camel.py
+++ camel.py
@@ -1,3 +1,8 @@
+'''In a file called camel.py, implement a program that prompts the user for the name of a 
+variable in camel case and outputs the corresponding name in snake case. Assume that the 
+user’s input will indeed be in camel case.
+'''
+
 def main():
     camel = (input("camelCase:"))
 
