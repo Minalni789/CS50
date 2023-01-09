eefb5aed43a9cb5fad649835a6cf700fa92de717
Minalni789 <122231159+Minalni789@users.noreply.github.com>
2023-01-09 17:47:50 +0200

    Added Description

--- einstein.py
+++ einstein.py
@@ -1,3 +1,8 @@
+'''In a file called einstein.py, implement a program in Python that prompts the user
+for mass as an integer (in kilograms) and then outputs the equivalent number of
+Joules as an integer. Assume that the user will input an integer.  
+'''
+
 def main():
     m = int(input("> "))
     E = m * 300000000 * 300000000
