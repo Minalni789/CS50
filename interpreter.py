3e775bdac71f59c929b430ca0859ecb2e44cc020
Minalni789 <122231159+Minalni789@users.noreply.github.com>
2023-01-09 18:05:51 +0200

    Added Description

--- interpreter.py
+++ interpreter.py
@@ -1,3 +1,9 @@
+'''In a file called interpreter.py, implement a program that prompts the user
+for an arithmetic expression and then calculates and outputs the result as a 
+floating-point value formatted to one decimal place. Assume that the user’s 
+input will be formatted as x y z, with one space between x and y and one space between y and z ''' 
+
+
 def main():
     take_input = input("Expression: ")
     x, y, z = take_input.split(" ")
